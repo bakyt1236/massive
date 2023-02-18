fd45f647ed7623764f4958964d337646cafbc9c8
bakyt1236 <apsamaov1@gmail.com>
2023-02-18 19:18:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a= scanner.nextInt();
+        int[]array=new int[a];
+        array[0]=21;
+        array[1]=1312;
+        array[2]=4114124;
+        array[3]=2;
+        array[4]=23;
+        array[5]=970697;
+        array[6]=2532;
+        array[7]=886;
+        array[8]=257735;
+        array[9]=22424;
+        System.out.println(array[6]);
+
+
+    }
+}
